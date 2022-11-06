7217c68d8cf5d507df195f4cfc13f40dcb396071
JamesA <97204146+jamesafonja@users.noreply.github.com>
2022-11-06 10:51:19 -0600

    Initial Commit

---
+++ MealsSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MealsSwiftUI
+//
+//  Created by Revive on 11/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MealsSwiftUI/MealsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealsSwiftUIApp.swift
+//  MealsSwiftUI
+//
+//  Created by Revive on 11/6/22.
+//
+
+import SwiftUI
+
+@main
+struct MealsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
